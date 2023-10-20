943635e6b1ed3b16a77abbd782e61d57061b23cc
ericgpks <sq.wacbc5.nyc@gmail.com>
2023-10-20 22:41:47 +0900

    add first test

---
+++ spec/requests/static_page_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe StaticPagesController do
+  describe 'Static pages' do
+    it 'should get home' do
+      get static_pages_home_url
+      assert_response :success
+    end
+
+    it 'should get help' do
+      get static_pages_help_url
+      assert_response :success
+    end
+  end
+end
